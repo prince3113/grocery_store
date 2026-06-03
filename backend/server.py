e327034c8ac6f15efe3c5fb5ba4aa48d45d1a042
prince <princesethia012331@gmail.com>
2026-06-03 11:23:14 +0530

    Migrated database from Mysql to Postgresql

--- backend/server.py
+++ backend/server.py
@@ -220,6 +220,5 @@ def login():
         "username": user["username"]
     })
 
-if __name__ == '__main__':
-    print("Starting Python Flask Server For Grocery Store Management System")
-    app.run(port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
